1b0df413cfab721c0677613edd7e031b5c289899
박시은 <charm516@naver.com>
2025-11-25 20:04:06 +0900

    [chore] - change mockData id to string

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,18 +1,18 @@
 export const mockData = [
   {
-    id: 0,
+    id: "0",
     isDone: false,
     content: "React 공부하기",
     date: new Date().getTime(),
   },
   {
-    id: 1,
+    id: "1",
     isDone: false,
     content: "블로그 정리하기",
     date: new Date().getTime(),
   },
   {
-    id: 2,
+    id: "2",
     isDone: true,
     content: "다이어리 쓰기",
     date: new Date().getTime(),
