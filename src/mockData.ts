3aa4d8ebdbb3fe28a420f782458c436417c169e5
박시은 <charm516@naver.com>
2025-11-25 16:57:55 +0900

    [chore] - mock date 생성

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,14 +1,20 @@
 export const mockData = [
   {
-    id: 1,
+    id: 0,
     isDone: false,
     content: "React 공부하기",
     date: new Date().getTime(),
   },
   {
-    id: 2,
+    id: 1,
     isDone: false,
-    content: "React 공부하기",
+    content: "블로그 정리하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: true,
+    content: "다이어리 쓰기",
     date: new Date().getTime(),
   },
 ];
