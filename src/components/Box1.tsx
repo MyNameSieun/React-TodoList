350aa1cd029bab82f3db31a18b28c70bcce6a520
박시은 <charm516@naver.com>
2025-11-27 14:14:33 +0900

    [delete] - delete components box

--- src/components/Box1.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Box1 = () => {
-  console.log("Box1 렌더링");
-  return <div className="h-[100px] w-[100px] bg-red-300">Box1</div>;
-};
-
-export default React.memo(Box1);
--- src/components/Box3.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Box3 = () => {
-  console.log("Box3 렌더링");
-  return <div className="h-[100px] w-[100px] bg-green-300">Box1</div>;
-};
-
-export default React.memo(Box3);
